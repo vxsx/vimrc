98077b20242f909b605ec5219c51a719064cfc1b
Vadim Sikora <vadim.sikora@gmail.com>
2014-03-16 00:47:11 +0100

    update script now updates everything correctly

--- update.sh
+++ update.sh
@@ -2,5 +2,7 @@
 echo "Updating main repo"
 cd ~/.vim && git pull
 echo ""
-echo "Updating plugins"
+echo "Updating vundle"
 cd ~/.vim/ && git submodule foreach git pull origin master
+echo "Updating plugins"
+vim -c ':BundleInstall!' -c ':qa'
